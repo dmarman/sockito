e20851a4cd83c9bba0d4180a6992f747101846b2
dmarman <dmm.martin@gmail.com>
2017-01-24 13:19:21 +0100

    Add routes.js

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-var MessageController = require('./MessageController');
+var MessageController = require('./Controllers/MessageController');
 
 var routes = function(socket){
 
@@ -6,8 +6,7 @@ var routes = function(socket){
     
     socket.emit('news', MessageController.goodbye());
     
-    socket.on('my other event',
-        (data) => {
+    socket.on('my other event', function(data){
             MessageController.response(data);
         });
     
